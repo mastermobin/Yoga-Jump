e5f574966354c5d22c21b18254a5d0384b2757ef
Unknown <vahdatitorkha.mobin@mail.um.ac.ir>
2019-04-30 13:15:31 +0430

    Move Yoga added

--- Assets/Scripts/MainLogic.cs
+++ Assets/Scripts/MainLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,5 +18,31 @@ public class MainLogic : MonoBehaviour
         {
             _animator.SetTrigger("Jump");
         }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0f, 0f, 0.05f);
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0f, 0f, -0.05f);
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.05f, 0f, 0f);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.05f, 0f, 0f);
+        }
+    }
+    
+    void OnCollisionEnter (Collision col)
+    {
+        Destroy(this);
+        Debug.Log("hey");
     }
 }
